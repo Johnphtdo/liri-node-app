1f8063d503c19ff934dcc9f98f14329f4cedafe5
Dohn Joe <johnphtdo@gmail.com>
2019-10-14 10:52:35 -0500

    spotify search is functioning

--- liri.js
+++ liri.js
@@ -7,11 +7,11 @@ var moment = require("moment");
 var axios = require("axios");
 // Variables to store input information
 var command = process.argv[2];
-var input = process.argv.slice(3)
+var input = process.argv.slice(3);
 var queryUrl = "";
 // Creating functions for each command
 function concertSearch() {
-  input = input.join("+")
+  input = input.join("+");
   queryUrl =
     "https://rest.bandsintown.com/artists/" +
     input +
@@ -52,7 +52,35 @@ function concertSearch() {
     });
 }
 function spotifySearch() {
-
+  input = input.join(" ");
+  spotifyClient.search({ type: "track", query: input, limit: 1 }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    var spotifyData = data.tracks.items[0];
+    var artistName = spotifyData.album.artists[0].name;
+    var songName = spotifyData.name;
+    var albumName = spotifyData.album.name;
+    var previewURL = spotifyData.preview_url;
+    // console.log(spotifyData.album.name)
+    // console.log (spotifyData.name)
+    // console.log(spotifyData);
+    // console.log(artistName);
+    // console.log(spotifyData.preview_url)
+    console.log(
+      "Artist Name: " +
+        artistName +
+        "\nSong Name: " +
+        songName +
+        "\nAlbum Name: " +
+        albumName +
+        "\nPreview URL: " +
+        previewURL
+    );
+  });
 }
 // Switch to run through the different commands
 switch (command) {
@@ -62,6 +90,7 @@ switch (command) {
     break;
   case "spotify-this-song":
     // console.log("Working");
+    spotifySearch();
     break;
   case "movie-this":
     // console.log("Working");
